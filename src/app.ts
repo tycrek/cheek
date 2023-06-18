e583290dd108ea59ed16fb74ab2fdf514ff0be54
tycrek <gpg+github@jmoore.dev>
2023-06-18 14:42:19 -0600

    feat: set up bindings

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,20 @@ import { Hono, Context } from 'hono';
  * Bindings introduced for Hono v3.0.0
  */
 type Bindings = {
+	/**
+	 * Static asset fetcher for Pages
+	 */
+	ASSETS: Fetcher;
+
+	/**
+	 * R2 Bucket for storing files
+	 */
+	cheekstore: R2Bucket;
+
+	/**
+	 * Cloudflare KV for storing credentials
+	 */
+	cheekkv: KVNamespace;
 }
 
 /**
